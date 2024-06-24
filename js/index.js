62f6c1ce3fb1a2affa8cf8a47292dcded2a37079
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-24 19:39:06 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,74 @@
+//forEach metodiga oid masalalar
+/* 
+//1
+function IkkigaKopaytirish(params) {
+  params.forEach(function (value, index) {
+    params[index] = value * 2;
+  });
+  return params;
+}
+
+console.log(IkkigaKopaytirish([1, 2, 3, 4]));
+
+// 2
+function KattaHarf(params) {
+  params.forEach(function (params) {
+    console.log(params.toUpperCase());
+  });
+}
+
+KattaHarf(["apple", "banana", "cherry"]);
+
+// 3
+function beshQoshish(numbers) {
+  const result = [];
+
+  numbers.forEach(function (value) {
+    {
+      const sum = value + 5;
+      result.push(sum);
+    }
+  });
+
+  return result;
+}
+
+console.log(beshQoshish([1, 2, 3, 4, 5]));
+*/
+
+// Map metodiga oid masalalar
+
+/*
+//1
+function IkkigaKopaytirish(params) {
+  const result = params.map(function (value) {
+    return value * 2;
+  });
+  return result;
+}
+
+console.log(IkkigaKopaytirish([1, 2, 3, 4]));
+//2
+function KattaHarf(params) {
+  params.map(function (params) {
+    console.log(params.toUpperCase());
+  });
+}
+
+KattaHarf(["apple", "banana", "cherry"]);
+// 3
+function beshQoshish(numbers) {
+  const result = [];
+
+  numbers.map(function (value) {
+    {
+      const sum = value + 5;
+      result.push(sum);
+    }
+  });
+
+  return result;
+}
+
+console.log(beshQoshish([1, 2, 3, 4, 5]));
+*/
